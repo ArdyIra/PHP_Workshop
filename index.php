b2a1e0c2dca9e5777d83788e4dd25259b8e6af35
ArdyIra <135177050+ArdyIra@users.noreply.github.com>
2023-10-27 13:54:16 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Da titul</title>
+</head>
+
+<body>
+	<header>
+		Hallo, freunde!
+	</header>
+	<?php
+	// Enter your code here, enjoy!
+	function daten(){
+		date_default_timezone_set("Europe/Riga");
+		$date = date('Y/m/d H:i:s');
+		return $date;
+	}
+	?>
+	<footer>
+		&copy; Copyright 2023 iAmCopiedRight
+		<br>
+		<?php
+			echo daten()
+		?>
+	</footer>
+</body>
+
+
+</html>
+
 
