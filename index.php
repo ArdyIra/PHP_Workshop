efb93e17a6024ed28b2f4ffa6dceb7e07d5d1dd5
ArdyIra <135177050+ArdyIra@users.noreply.github.com>
2023-10-27 15:27:41 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,16 +6,48 @@
 
 <body>
 	<header>
-		Hallo, freunde!
+		<h1>Hallo, freunde!</h1>
 	</header>
+    
 	<?php
-	// Enter your code here, enjoy!
 	function daten(){
-		date_default_timezone_set("Europe/Riga");
+    date_default_timezone_set("Europe/Riga");
 		$date = date('Y/m/d H:i:s');
 		return $date;
 	}
-	?>
+    $name = $surname = $dateOfBirth = "";
+	
+    
+    
+    ?>
+    
+    <form method="post" action="">  
+  Name: <input type="text" name="name" value="<?php echo $name;?>">
+  <br><br>
+  Surname: <input type="text" name="surname" value="<?php echo $surname;?>">
+  <br><br>
+  Date of Birth: <input type="text" name="date_of_birth" value="<?php echo $dateOfBirth;?>">
+
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
+    
+<?php
+
+$name = $_POST['name'];
+$surname = $_POST['surname'];
+$dateOfBirth = $_POST['date_of_birth'];
+
+
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $surname;
+echo "<br>";
+echo $dateOfBirth;
+echo "<br>";
+?>    
+    
 	<footer>
 		<section id="copyright">
 			&copy; Copyright 2023 iAmCopiedRight
